e1411c80a9770da31d6ceff65739d5c4c9b1ee2e
Jonathan Herrera <Jonathan.a.herrera0@gmail.com>
2024-08-29 23:49:09 -0500

    Populate header

--- flashcardai/app/page.js
+++ flashcardai/app/page.js
@@ -1,6 +1,8 @@
 "use client"
-import {Box} from '@mui/material';
+import {Box, Button, IconButton} from '@mui/material';
 import {createTheme, ThemeProvider} from '@mui/material/styles'
+import Menu from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle'
 
 const theme = createTheme({
     palette: {
@@ -10,21 +12,50 @@ const theme = createTheme({
       secondary: {
         main: '#ea80fc',
       },
+      white: {
+        main: '#ffffff'
+      }
     },
 });
 
 export default function Home() {
+
+
   return (
-    <Box
-    width={"100vw"}
-    height={"100vw"}
-    display={'flex'}
-    flexDirection={'column'}
-    bgcolor={'#000'}
-    >
-      <Box>
-        <Box width={'100%'} height={"30px"} bgcolor={theme.palette.primary.main}></Box>
+    <ThemeProvider theme = {theme}>
+      <Box
+      width={"100vw"}
+      height={"100vw"}
+      display={'flex'}
+      flexDirection={'column'}
+      bgcolor={'#000'}
+      >
+        <Box>
+          <Box 
+          width={'100%'} 
+          height={"50px"} 
+          bgcolor={'primary.main'} 
+          display={'flex'} 
+          padding={'20px'}
+          justifyContent={'space-between'} 
+          alignItems={'center'}
+          color={'white'}>
+            <Box display={'flex'} gap={'20px'}>
+              <Box color='secondary.main'> Logo </Box>
+              <Box color='secondary.main'> AIFlashCards </Box>
+            </Box>
+            <Box display={'flex'} justifyContent={'center'} alignItems={'center'} gap={'20px'}>
+              <Button color='secondary' variant='text' > Menu 1 </Button>
+              <Button color='secondary' variant='text'> Menu 2 </Button>
+              <Button color='secondary' variant='text'> Menu 3 </Button>
+              <IconButton aria-label='Menu' color='secondary'> <Menu /> </IconButton>
+              <IconButton aria-label='AccountCircle' color='secondary'> <AccountCircle /> </IconButton>
+            </Box>
+            
+          </Box>
+        </Box>
+        <Box width={'100px'} height={'100px'} bgcolor={'secondary.main'} fle></Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
